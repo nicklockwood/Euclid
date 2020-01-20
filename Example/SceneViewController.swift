73fdb4c1b42bada132b612b7f5ee9b00d4f1450c
Nick Lockwood <nick@charcoaldesign.co.uk>
2020-01-20 18:41:38 +0000

    Increased example poly count and added timing

--- Example/SceneViewController.swift
+++ Example/SceneViewController.swift
@@ -26,9 +26,12 @@ class SceneViewController: UIViewController {
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
 
         // create some geometry using Euclid
+        let start = CFAbsoluteTimeGetCurrent()
         let cube = Mesh.cube(size: 0.8, material: UIColor.red)
-        let sphere = Mesh.sphere(slices: 40, material: UIColor.blue)
+        let sphere = Mesh.sphere(slices: 120, material: UIColor.blue)
         let mesh = cube.subtract(sphere)
+        print("Time:", CFAbsoluteTimeGetCurrent() - start)
+        print("Polys:", mesh.polygons.count)
 
         // create SCNNode
         let geometry = SCNGeometry(mesh) {
