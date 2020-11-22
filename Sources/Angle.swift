98348b28ad0119ff221a98b37340f7843f2e3039
jkaliak <john@kaliakatsos.net>
2020-11-22 10:58:36 +0100

    Allow only one constructor

--- Sources/Angle.swift
+++ Sources/Angle.swift
@@ -10,29 +10,25 @@ import Foundation
 
 /// A type-safe struct for all API related to angles
 public struct Angle {
-    fileprivate static let radiansPerDegree = Double.pi / 180
+    fileprivate static let degreesPerRadian = 180 / Double.pi
 
-    public let radians: Double
-
-    public init(radians: Double) {
-        self.radians = radians
-    }
+    public let degrees: Double
 
     public init(degrees: Double) {
-        self.radians = degrees * Angle.radiansPerDegree
+        self.degrees = degrees
     }
 }
 
 public extension Angle {
-    var degrees: Double { radians / Angle.radiansPerDegree }
+    var radians: Double { degrees / Angle.degreesPerRadian }
 }
 
 public extension Angle {
     static var zero = Angle(degrees: 0)
 
-    static var pi = Angle(radians: .pi)
+    static var pi = Angle(degrees: 180)
 
-    static var twoPi = Angle(radians: 2 * .pi)
+    static var twoPi = Angle(degrees: 360)
 }
 
 public extension Angle {
