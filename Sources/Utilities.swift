698dd4e58234106191d0ef67a0a2bb2dd33f3c74
Nick Lockwood <nick@charcoaldesign.co.uk>
2021-08-15 07:07:49 +0100

    Skip pointsAreSelfIntersecting() check if points.count < 4

--- Sources/Utilities.swift
+++ Sources/Utilities.swift
@@ -248,6 +248,10 @@ func pointsAreConvex(_ points: [Vector]) -> Bool {
 // Test if path is self-intersecting
 // TODO: optimize by using http://www.webcitation.org/6ahkPQIsN
 func pointsAreSelfIntersecting(_ points: [Vector]) -> Bool {
+    guard points.count > 2 else {
+        // A triangle can't be self-intersecting
+        return false
+    }
     for i in 0 ..< points.count - 2 {
         let p0 = points[i]
         let p1 = points[i + 1]
