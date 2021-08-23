c89735d9b014fdb7f2d31c7fe76b89431f580c75
jkaliak <john@kaliakatsos.net>
2021-08-23 21:58:24 +0200

    the inverse of a rotation matrix is its own transpose

--- Sources/Rotation.swift
+++ Sources/Rotation.swift
@@ -305,32 +305,12 @@ internal extension Rotation {
             - m13 * m22 * m31
     }
 
-    var adjugate: Rotation {
-        Rotation(
-            m22 * m33 - m23 * m32,
-            m13 * m32 - m12 * m33,
-            m12 * m23 - m13 * m22,
-            m23 * m31 - m21 * m33,
-            m11 * m33 - m13 * m31,
-            m13 * m21 - m11 * m23,
-            m21 * m32 - m22 * m31,
-            m12 * m31 - m11 * m32,
-            m11 * m22 - m12 * m21
-        )
-    }
-
     var transpose: Rotation {
         Rotation(m11, m21, m31, m12, m22, m32, m13, m23, m33)
     }
 
     var inverse: Rotation {
-        let a = adjugate
-        let d = determinant
-        return Rotation(
-            a.m11 / d, a.m12 / d, a.m13 / d,
-            a.m21 / d, a.m22 / d, a.m23 / d,
-            a.m31 / d, a.m32 / d, a.m33 / d
-        )
+        transpose
     }
 
     var isRotationMatrix: Bool {
