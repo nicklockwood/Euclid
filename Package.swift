e12bfd60b85d679a8f05d930fad4f761f9e5217e
Joe Heck <heckj@mac.com>
2022-02-06 10:42:29 -0800

    rendering docs (Swift 5.6 ONLY)

--- Package.swift
+++ Package.swift
@@ -1,11 +1,16 @@
-// swift-tools-version:5.1
-
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "Euclid",
     products: [
         .library(name: "Euclid", targets: ["Euclid"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        // Swift-DocC Plugin - swift 5.6 ONLY (GitHhub Actions on 1/29/2022 only supports to 5.5)
+        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
     ],
     targets: [
         .target(name: "Euclid", path: "Sources"),
