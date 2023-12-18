87f526e1953dc1c63fdcd5ad83257448741bb855
Nick Lockwood <nick@charcoaldesign.co.uk>
2023-12-18 18:45:28 +0000

    Add STL import

--- Sources/Mesh+STL.swift
+++ Sources/Mesh+STL.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+private let headerSize = 80
+private let triangleSize = 50
+
+// MARK: Export
+
 public extension Mesh {
     /// Return ASCII STL string data for the mesh.
     func stlString(name: String) -> String {
@@ -29,8 +34,6 @@ public extension Mesh {
     /// - Parameter colorLookup: A closure to map Euclid materials to STL facet colors. Use `nil` for default mapping.
     /// - Returns: A Euclid `Color` value.
     func stlData(colorLookup: STLColorProvider? = nil) -> Data {
-        let headerSize = 80
-        let triangleSize = 50
         let triangles = triangulate().polygons
         let bufferSize = headerSize + 4 + triangles.count * triangleSize
         let buffer = Buffer(capacity: bufferSize)
@@ -121,4 +124,176 @@ private func defaultColorMapping(_ material: Polygon.Material?) -> Color? {
     }
     #endif
     return nil
+}
+
+// MARK: Import
+
+public extension Mesh {
+    /// Create a mesh from an STL string.
+    /// - Parameter stlString: ASCII STL string data.
+    init?(stlString: String) {
+        var lines = ArraySlice(stlString.components(separatedBy: .newlines))
+        guard let mesh = lines.readSolid() else {
+            return nil
+        }
+        self = mesh
+    }
+
+    /// A closure that maps an STL facet color to a Euclid material.
+    /// - Parameter c: A Euclid `Color` to convert, or `nil` for the default color.
+    /// - Returns: A Euclid `Material` value, or `nil` for the default material.
+    typealias STLMaterialProvider = (_ c: Color?) -> Material?
+
+    /// Create a mesh from STL data.
+    /// - Parameters
+    ///   - stlData: binary or ASCII STL file data
+    ///   - materialLookup: A closure to map STL facet colors to Euclid materials. Use `nil` for default mapping.
+    init?(stlData: Data, materialLookup: STLMaterialProvider? = nil) {
+        if stlData.count >= 5,
+           let prefix = String(data: stlData[0 ..< 5], encoding: .utf8),
+           prefix.caseInsensitiveCompare("solid") == .orderedSame,
+           let string = String(data: stlData, encoding: .utf8)
+        {
+            self.init(stlString: string)
+            return
+        }
+        var offset = headerSize
+        guard stlData.count >= offset + 4 else {
+            return nil
+        }
+        let count = Int(stlData.withUnsafeBytes { $0.readUInt32(at: &offset) })
+        guard stlData.count >= offset + count * triangleSize else {
+            return nil
+        }
+        let materialLookup = materialLookup ?? { $0 }
+        let triangles = stlData.withUnsafeBytes { buffer -> [Polygon] in
+            (0 ..< count).compactMap { _ in
+                buffer.readTriangle(at: &offset, materialLookup: materialLookup)
+            }
+        }
+        self.init(triangles)
+    }
+}
+
+private extension ArraySlice where Element == String {
+    mutating func readCommand(_ name: String, parts: inout [String]) -> Bool {
+        var line = ""
+        guard readCommand(name, line: &line) else {
+            return false
+        }
+        parts = line[name.endIndex...]
+            .components(separatedBy: .whitespaces)
+            .map { $0.trimmingCharacters(in: .whitespaces) }
+            .filter { !$0.isEmpty }
+        return true
+    }
+
+    mutating func readCommand(_ name: String) -> Bool {
+        var line = ""
+        return readCommand(name, line: &line)
+    }
+
+    mutating func readCommand(_ name: String, line: inout String) -> Bool {
+        while let line = first?.trimmingCharacters(in: .whitespaces), line.isEmpty {
+            removeFirst()
+        }
+        line = first?.trimmingCharacters(in: .whitespaces) ?? ""
+        if line.prefix(name.count).caseInsensitiveCompare(name) == .orderedSame {
+            removeFirst()
+            return true
+        }
+        return false
+    }
+
+    mutating func readVertex() -> Vertex? {
+        var parts = [String]()
+        guard readCommand("vertex", parts: &parts) else {
+            return nil
+        }
+        return Vertex(parts.compactMap(Double.init))
+    }
+
+    mutating func readTriangle() -> Polygon? {
+        guard readCommand("facet"),
+              readCommand("outer"),
+              let a = readVertex(),
+              let b = readVertex(),
+              let c = readVertex(),
+              readCommand("endloop"),
+              readCommand("endfacet")
+        else {
+            return nil
+        }
+        return Polygon([a, b, c])
+    }
+
+    mutating func readSolid() -> Mesh? {
+        guard readCommand("solid") else {
+            return nil
+        }
+        var triangles = [Polygon]()
+        while let triangle = readTriangle() {
+            triangles.append(triangle)
+        }
+        guard readCommand("endsolid") else {
+            return nil
+        }
+        return Mesh(triangles)
+    }
+}
+
+private extension UnsafeRawBufferPointer {
+    #if swift(<5.7)
+    func loadUnaligned<T>(fromByteOffset offset: Int = 0, as type: T.Type) -> T {
+        // Note: this polyfill implementation only works for 4-byte types
+        let source = baseAddress.map(UnsafeRawPointer.init)!
+        var storage: UInt32 = 0
+        return withUnsafeMutablePointer(to: &storage) {
+            let raw = UnsafeMutableRawPointer($0)
+            raw.copyMemory(from: source + offset, byteCount: 4)
+            return raw.load(as: type)
+        }
+    }
+    #endif
+
+    func readUInt16(at offset: inout Int) -> UInt16 {
+        defer { offset += 2 }
+        return load(fromByteOffset: offset, as: UInt16.self)
+    }
+
+    func readUInt32(at offset: inout Int) -> UInt32 {
+        defer { offset += 4 }
+        return loadUnaligned(fromByteOffset: offset, as: UInt32.self)
+    }
+
+    func readFloat(at offset: inout Int) -> Float {
+        defer { offset += 4 }
+        return loadUnaligned(fromByteOffset: offset, as: Float.self)
+    }
+
+    func readVector(at offset: inout Int) -> Vector {
+        .init(
+            Double(readFloat(at: &offset)),
+            Double(readFloat(at: &offset)),
+            Double(readFloat(at: &offset))
+        )
+    }
+
+    func readColor(at offset: inout Int) -> Color? {
+        let color = readUInt16(at: &offset)
+        guard color & 0x8000 > 0 else {
+            return nil
+        }
+        let blue = Double(color & 31) / 31
+        let green = Double((color & (31 << 5)) >> 5) / 31
+        let red = Double((color & (31 << 10)) >> 10) / 31
+        return .init(red, green, blue)
+    }
+
+    func readTriangle(at offset: inout Int, materialLookup: Mesh.STLMaterialProvider) -> Polygon? {
+        _ = readVector(at: &offset) // Normal (ignored)
+        let vertices = (0 ..< 3).map { _ in readVector(at: &offset) }
+        let color = readColor(at: &offset)
+        return Polygon(vertices, material: materialLookup(color))
+    }
 }
---
+++ Tests/MeshImportTests.swift
@@ -0,0 +1,363 @@
+//
+//  MeshImportTests.swift
+//  EuclidTests
+//
+//  Created by Nick Lockwood on 17/12/2023.
+//  Copyright © 2023 Nick Lockwood. All rights reserved.
+//
+
+@testable import Euclid
+import XCTest
+
+private extension Data {
+    init(hexString: String) {
+        var bytes = [UInt8]()
+        var index = hexString.startIndex
+        while let next = hexString.index(index, offsetBy: 2, limitedBy: hexString.endIndex) {
+            UInt8(hexString[index ..< next], radix: 16).map { bytes.append($0) }
+            index = next
+        }
+        self.init(bytes)
+    }
+}
+
+class MeshImportTests: XCTestCase {
+    // MARK: STL import
+
+    func testCubeSTL() {
+        let cube = Mesh(stlString: """
+        solid Foo
+        facet normal 1 0 0
+        \touter loop
+        \t\tvertex 1 0 1
+        \t\tvertex 1 0 0
+        \t\tvertex 1 1 0
+        \tendloop
+        endfacet
+        facet normal 1 0 0
+        \touter loop
+        \t\tvertex 1 0 1
+        \t\tvertex 1 1 0
+        \t\tvertex 1 1 1
+        \tendloop
+        endfacet
+        facet normal -1 0 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 0 0 1
+        \t\tvertex 0 1 1
+        \tendloop
+        endfacet
+        facet normal -1 0 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 0 1 1
+        \t\tvertex 0 1 0
+        \tendloop
+        endfacet
+        facet normal 0 1 0
+        \touter loop
+        \t\tvertex 0 1 1
+        \t\tvertex 1 1 1
+        \t\tvertex 1 1 0
+        \tendloop
+        endfacet
+        facet normal 0 1 0
+        \touter loop
+        \t\tvertex 0 1 1
+        \t\tvertex 1 1 0
+        \t\tvertex 0 1 0
+        \tendloop
+        endfacet
+        facet normal 0 -1 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 1 0 0
+        \t\tvertex 1 0 1
+        \tendloop
+        endfacet
+        facet normal 0 -1 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 1 0 1
+        \t\tvertex 0 0 1
+        \tendloop
+        endfacet
+        facet normal 0 0 1
+        \touter loop
+        \t\tvertex 0 0 1
+        \t\tvertex 1 0 1
+        \t\tvertex 1 1 1
+        \tendloop
+        endfacet
+        facet normal 0 0 1
+        \touter loop
+        \t\tvertex 0 0 1
+        \t\tvertex 1 1 1
+        \t\tvertex 0 1 1
+        \tendloop
+        endfacet
+        facet normal 0 0 -1
+        \touter loop
+        \t\tvertex 1 0 0
+        \t\tvertex 0 0 0
+        \t\tvertex 0 1 0
+        \tendloop
+        endfacet
+        facet normal 0 0 -1
+        \touter loop
+        \t\tvertex 1 0 0
+        \t\tvertex 0 1 0
+        \t\tvertex 1 1 0
+        \tendloop
+        endfacet
+        endsolid Foo
+        """)
+        let expected = Mesh.cube()
+            .translated(by: Vector(0.5, 0.5, 0.5))
+            .withoutTexcoords()
+            .triangulate()
+        XCTAssertEqual(cube, expected)
+    }
+
+    func testUppercaseCubeSTL() {
+        let cube = Mesh(stlString: """
+        SOLID FOO
+        FACET NORMAL 1 0 0
+        \tOUTER LOOP
+        \t\tVERTEX 1 0 1
+        \t\tVERTEX 1 0 0
+        \t\tVERTEX 1 1 0
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 1 0 0
+        \tOUTER LOOP
+        \t\tVERTEX 1 0 1
+        \t\tVERTEX 1 1 0
+        \t\tVERTEX 1 1 1
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL -1 0 0
+        \tOUTER LOOP
+        \t\tVERTEX 0 0 0
+        \t\tVERTEX 0 0 1
+        \t\tVERTEX 0 1 1
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL -1 0 0
+        \tOUTER LOOP
+        \t\tVERTEX 0 0 0
+        \t\tVERTEX 0 1 1
+        \t\tVERTEX 0 1 0
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 1 0
+        \tOUTER LOOP
+        \t\tVERTEX 0 1 1
+        \t\tVERTEX 1 1 1
+        \t\tVERTEX 1 1 0
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 1 0
+        \tOUTER LOOP
+        \t\tVERTEX 0 1 1
+        \t\tVERTEX 1 1 0
+        \t\tVERTEX 0 1 0
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 -1 0
+        \tOUTER LOOP
+        \t\tVERTEX 0 0 0
+        \t\tVERTEX 1 0 0
+        \t\tVERTEX 1 0 1
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 -1 0
+        \tOUTER LOOP
+        \t\tVERTEX 0 0 0
+        \t\tVERTEX 1 0 1
+        \t\tVERTEX 0 0 1
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 0 1
+        \tOUTER LOOP
+        \t\tVERTEX 0 0 1
+        \t\tVERTEX 1 0 1
+        \t\tVERTEX 1 1 1
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 0 1
+        \tOUTER LOOP
+        \t\tVERTEX 0 0 1
+        \t\tVERTEX 1 1 1
+        \t\tVERTEX 0 1 1
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 0 -1
+        \tOUTER LOOP
+        \t\tVERTEX 1 0 0
+        \t\tVERTEX 0 0 0
+        \t\tVERTEX 0 1 0
+        \tENDLOOP
+        ENDFACET
+        FACET NORMAL 0 0 -1
+        \tOUTER LOOP
+        \t\tVERTEX 1 0 0
+        \t\tVERTEX 0 1 0
+        \t\tVERTEX 1 1 0
+        \tENDLOOP
+        ENDFACET
+        ENDSOLID FOO
+        """)
+        let expected = Mesh.cube()
+            .translated(by: Vector(0.5, 0.5, 0.5))
+            .withoutTexcoords()
+            .triangulate()
+        XCTAssertEqual(cube, expected)
+    }
+
+    func testCubeSTLData() {
+        let data = Data(hexString: """
+        000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+        00000000000000000000000000000000000000000000000000c0000000000803f00000000000000000000803f000000000000803f000080\
+        3f00000000000000000000803f0000803f0000000000000000803f00000000000000000000803f000000000000803f0000803f0000803f0\
+        00000000000803f0000803f0000803f0000000080bf000000800000008000000000000000000000000000000000000000000000803f0000\
+        00000000803f0000803f0000000080bf0000008000000080000000000000000000000000000000000000803f0000803f000000000000803\
+        f000000000000000000000000803f00000000000000000000803f0000803f0000803f0000803f0000803f0000803f0000803f0000000000\
+        00000000000000803f00000000000000000000803f0000803f0000803f0000803f00000000000000000000803f000000000000000000800\
+        00080bf000000800000000000000000000000000000803f00000000000000000000803f000000000000803f000000000080000080bf0000\
+        00800000000000000000000000000000803f000000000000803f00000000000000000000803f000000000000000000000000803f0000000\
+        0000000000000803f0000803f000000000000803f0000803f0000803f0000803f000000000000000000000000803f000000000000000000\
+        00803f0000803f0000803f0000803f000000000000803f0000803f00000000008000000080000080bf0000803f000000000000000000000\
+        0000000000000000000000000000000803f0000000000000000008000000080000080bf0000803f0000000000000000000000000000803f\
+        000000000000803f0000803f000000000000
+        """)
+        let cube = Mesh(stlData: data)
+        let expected = Mesh.cube()
+            .translated(by: Vector(0.5, 0.5, 0.5))
+            .withoutTexcoords()
+            .triangulate()
+        XCTAssertEqual(cube, expected)
+    }
+
+    func testRedCubeSTLData() {
+        let data = Data(hexString: """
+        000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+        00000000000000000000000000000000000000000000000000c0000000000803f00000000000000000000803f000000000000803f000080\
+        3f00000000000000000000803f0000803f0000000000fc0000803f00000000000000000000803f000000000000803f0000803f0000803f0\
+        00000000000803f0000803f0000803f00fc000080bf000000800000008000000000000000000000000000000000000000000000803f0000\
+        00000000803f0000803f00fc000080bf0000008000000080000000000000000000000000000000000000803f0000803f000000000000803\
+        f0000000000fc000000000000803f00000000000000000000803f0000803f0000803f0000803f0000803f0000803f0000803f0000000000\
+        fc000000000000803f00000000000000000000803f0000803f0000803f0000803f00000000000000000000803f0000000000fc000000800\
+        00080bf000000800000000000000000000000000000803f00000000000000000000803f000000000000803f00fc00000080000080bf0000\
+        00800000000000000000000000000000803f000000000000803f00000000000000000000803f00fc00000000000000000000803f0000000\
+        0000000000000803f0000803f000000000000803f0000803f0000803f0000803f00fc00000000000000000000803f000000000000000000\
+        00803f0000803f0000803f0000803f000000000000803f0000803f00fc0000008000000080000080bf0000803f000000000000000000000\
+        0000000000000000000000000000000803f0000000000fc0000008000000080000080bf0000803f0000000000000000000000000000803f\
+        000000000000803f0000803f0000000000fc
+        """)
+        let cube = Mesh(stlData: data)
+        let expected = Mesh.cube(material: Color.red)
+            .translated(by: Vector(0.5, 0.5, 0.5))
+            .withoutTexcoords()
+            .triangulate()
+        XCTAssertEqual(cube, expected)
+    }
+
+    func testCubeSTLStringData() {
+        let data = """
+        solid Foo
+        facet normal 1 0 0
+        \touter loop
+        \t\tvertex 1 0 1
+        \t\tvertex 1 0 0
+        \t\tvertex 1 1 0
+        \tendloop
+        endfacet
+        facet normal 1 0 0
+        \touter loop
+        \t\tvertex 1 0 1
+        \t\tvertex 1 1 0
+        \t\tvertex 1 1 1
+        \tendloop
+        endfacet
+        facet normal -1 0 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 0 0 1
+        \t\tvertex 0 1 1
+        \tendloop
+        endfacet
+        facet normal -1 0 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 0 1 1
+        \t\tvertex 0 1 0
+        \tendloop
+        endfacet
+        facet normal 0 1 0
+        \touter loop
+        \t\tvertex 0 1 1
+        \t\tvertex 1 1 1
+        \t\tvertex 1 1 0
+        \tendloop
+        endfacet
+        facet normal 0 1 0
+        \touter loop
+        \t\tvertex 0 1 1
+        \t\tvertex 1 1 0
+        \t\tvertex 0 1 0
+        \tendloop
+        endfacet
+        facet normal 0 -1 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 1 0 0
+        \t\tvertex 1 0 1
+        \tendloop
+        endfacet
+        facet normal 0 -1 0
+        \touter loop
+        \t\tvertex 0 0 0
+        \t\tvertex 1 0 1
+        \t\tvertex 0 0 1
+        \tendloop
+        endfacet
+        facet normal 0 0 1
+        \touter loop
+        \t\tvertex 0 0 1
+        \t\tvertex 1 0 1
+        \t\tvertex 1 1 1
+        \tendloop
+        endfacet
+        facet normal 0 0 1
+        \touter loop
+        \t\tvertex 0 0 1
+        \t\tvertex 1 1 1
+        \t\tvertex 0 1 1
+        \tendloop
+        endfacet
+        facet normal 0 0 -1
+        \touter loop
+        \t\tvertex 1 0 0
+        \t\tvertex 0 0 0
+        \t\tvertex 0 1 0
+        \tendloop
+        endfacet
+        facet normal 0 0 -1
+        \touter loop
+        \t\tvertex 1 0 0
+        \t\tvertex 0 1 0
+        \t\tvertex 1 1 0
+        \tendloop
+        endfacet
+        endsolid Foo
+        """.data(using: .utf8)
+        let cube = Mesh(stlData: data!)
+        let expected = Mesh.cube()
+            .translated(by: Vector(0.5, 0.5, 0.5))
+            .withoutTexcoords()
+            .triangulate()
+        XCTAssertEqual(cube, expected)
+    }
+}
