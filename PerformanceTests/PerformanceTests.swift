b1dc6dc7c25a313eae63d089c16a7a3e95bc9adb
Nick Lockwood <nick@charcoaldesign.co.uk>
2025-09-01 07:39:42 +0100

    Add performance tests

---
+++ PerformanceTests/PerformanceTests.swift
@@ -0,0 +1,29 @@
+//
+//  PerformanceTests.swift
+//  PerformanceTests
+//
+//  Created by Nick Lockwood on 31/08/2025.
+//  Copyright © 2025 Nick Lockwood. All rights reserved.
+//
+
+import Euclid
+import XCTest
+
+final class PerformanceTests: XCTestCase {
+    func testMeshClipping() throws {
+        let detail = 64
+        let a = Mesh.sphere(slices: detail)
+        let b = a.translated(by: [0.5, 0, 0])
+        measure {
+            let c = a.withoutOptimizations().clipped(to: b.withoutOptimizations())
+            XCTAssertFalse(c.isEmpty)
+        }
+    }
+}
+
+private extension Mesh {
+    /// Remove cached BSP, isConvex, etc
+    func withoutOptimizations() -> Self {
+        Mesh(polygons)
+    }
+}
