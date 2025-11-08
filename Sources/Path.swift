0d200bc678ce81b48cdb86fa3d3468b3a7339467
Nick Lockwood <nick@charcoaldesign.co.uk>
2025-11-08 19:58:15 +0000

    Improve Path.edgeVertices logic

--- Sources/Path.swift
+++ Sources/Path.swift
@@ -378,8 +378,7 @@ public extension Path {
     /// - Parameter wrapMode: The wrap mode to use for generating texture coordinates.
     /// - Returns: The edge vertices, or an empty array if path has subpaths.
     func edgeVertices(for wrapMode: Mesh.WrapMode) -> [Vertex] {
-        guard subpaths.count <= 1, points.count >= 2 else {
-            // TODO: does this actually match the documented behavior?
+        guard subpaths.count <= 1, points.count > 1 else {
             return points.first.map { [Vertex($0)] } ?? []
         }
 
@@ -388,7 +387,7 @@ public extension Path {
         switch wrapMode {
         case .shrink, .default:
             var prev = points[0].position
-            totalLength = points.reduce(0) { total, point in
+            totalLength = points.dropFirst().reduce(0) { total, point in
                 defer { prev = point.position }
                 return total + point.distance(from: prev)
             }
